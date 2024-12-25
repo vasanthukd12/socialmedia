560021d0228a4e4aa22a68e5e91936e4dc5b67e4
Vasanth Kumar <vasanthukd12@gmail.com>
2024-12-25 13:03:12 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SocialMedia from './SocialMedia/SocialMedia';
+
+
+
+function App() {
+  return (
+      <div style={{margin:'0px',padding:'0px'}}>
+        <SocialMedia/>
+      </div>
+  );
+}
+
+export default App 
